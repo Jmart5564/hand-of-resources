7cffb6a32ed32becc07f42732b27807703157b29
Jmart5564 <Jmart5564@gmail.com>
2022-08-05 18:14:27 -0700

    added test for get pokemon by ID

--- __tests__/pokemon.test.js
+++ __tests__/pokemon.test.js
@@ -17,6 +17,16 @@ describe('pokemon routes', () => {
     expect(kadabra).toHaveProperty('pokedex_number', 64);
     expect(kadabra).toHaveProperty('type', 'Psychic');
   });
+
+  it('#GET /pokemon/1 should return a single pokemon with detail', async () => {
+    const resp = await request(app).get('/pokemon/1');
+    expect(resp.body).toEqual({ 
+      id: '1',
+      name: 'Kadabra',
+      pokedex_number: 64,
+      type: 'Psychic',
+    });
+  });
   afterAll(() => {
     pool.end();
   });
