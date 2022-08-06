8bf1d39830aeb823bcc1903f103c15664fae353a
Jmart5564 <Jmart5564@gmail.com>
2022-08-05 17:06:03 -0700

    get cat by id test passing

--- __tests__/cats.test.js
+++ __tests__/cats.test.js
@@ -18,6 +18,16 @@ describe('cats routes', () => {
     expect(cats).toHaveProperty('age', 2);
     expect(cats).toHaveProperty('owner', 'Colter');
   });
+  it('#GET /cats/1 should return a single cat with detail', async () => {
+    const resp = await request(app).get('/cats/1');
+    expect(resp.body).toEqual({ 
+      id: '1',
+      name: 'Momo',
+      color: 'Black',
+      age: 2,
+      owner: 'Colter' 
+    });
+  });
   afterAll(() => {
     pool.end();
   });
